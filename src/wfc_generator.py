39648c7e11d9631f52f12f4c05d7c6462daf5403
st167058 <st167058@stud.uni-stuttgart.de>
2023-01-23 23:35:53 +0100

    reworked to make use of the pattern class instead of a confusing set of list

--- src/wfc_generator.py
+++ src/wfc_generator.py
@@ -7,17 +7,31 @@ import image_translator
 import tile_model
 import directions
 
+
+
 class UnsolvableException(Exception):
     def __init__(self):
         super().__init__("At least one tile exists that has no matching pattern")
 
+class NotInitializedException(Exception):
+    def __init__(self, msg="unknown"):
+        super().__init__(f"Has not been initialized - <{msg}>")
+
+
 
 class WFCGenerator(object):
-    def __init__(self, tile_model):
+    def __init__(self, tile_model: tile_model.TileModel):
         self._tile_model = tile_model
+        self._output = None
+
+    @property
+    def output(self) -> list:
+        if self._output is None:
+            raise NotInitializedException("output")
+        return self._output
 
 
-    def _is_fully_collapsed(self):
+    def _is_fully_collapsed(self) -> bool:
         """
         Returns true when the algorithm finished and produced a valid output
         """
@@ -30,7 +44,7 @@ class WFCGenerator(object):
         return True
 
 
-    def _get_possible_patterns(self, pos):
+    def _get_possible_patterns(self, pos: tuple) -> list:
         """
         Returns all valid patters at specific position
         """
@@ -39,18 +53,18 @@ class WFCGenerator(object):
             raise ValueError(f"no possible patterns at {pos}")
         return patterns
 
-    def _get_shannon_entropy(self, pos):
+    def _get_shannon_entropy(self, pos: tuple) -> float:
         """
         Calculate the shannon entropy at a specific position
         Tiles with only one pattern available have 0 entropy
 
         """
         entropy = 0
-        if len(self._output[pos[0]][pos[1]]) == 1:
+        if len(self.output[pos[0]][pos[1]]) == 1:
             return entropy
 
-        for pattern in self._output[pos[0]][pos[1]]:
-            entropy += self._tile_model.probabilities[pattern] * math.log(self._tile_model.probabilities[pattern], 2)
+        for pattern in self.output[pos[0]][pos[1]]:
+            entropy += pattern.probability * math.log(pattern.probability, 2)
         entropy *= -1
 
         # Add some random noise for more natural distribution of collapse
@@ -58,17 +72,33 @@ class WFCGenerator(object):
         return entropy
     
 
-    def _get_minimum_entropy_position(self):
+    def _get_minimum_entropy_position(self) -> tuple:
         """
-        Returns the position with the smallest entropy
+        Returns the position with the smallest entropy, classical approach of looping over every element 
+        and overwriting the smallest element when smaller element has been found.
+        ! There will be minor differences when entropy table is printed afterwards because        !
+        ! of adding a little random offset to every value for a more natural generating algorithm !
         """
-        raise NotImplementedError()  
+        minimum_entropy = self._get_shannon_entropy((0, 0))
+        minimum_entropy_position = (0, 0)
+        for row in range(len(self.output)):
+            for col in range(len(self.output[0])):
+                if (entropy := self._get_shannon_entropy((row, col))) < minimum_entropy:
+                    minimum_entropy = entropy
+                    minimum_entropy_position = (row, col)
+        return minimum_entropy_position       
 
-    def _get_maximum_probability(self, pos):
+    def _get_maximum_probability(self, pos: tuple) -> int:
         """
-        Returns the highest probabilty a pattern can have at given position
+        Returns the highest probabilty a pattern can have at given position, classical approach of looping 
+        over every element and overwriting the the maximum element when bigger element has been found.
+        Pattern probability is constant for same input
         """
-        raise NotImplementedError()  
+        maximum_probability = 0
+        for pattern in self.ouput[pos[0]][pos[1]]:
+            if probability := pattern.probability > maximum_probability:
+                maximum_probability = probability
+        return probability
 
     def _collapse(self, pos):
         """
@@ -89,7 +119,8 @@ class WFCGenerator(object):
         """
         Generate a new tile map of given size, the tilemap can be post processed to fill in corresponding patterns
         """
-        raise NotImplementedError()  
+        self._init_output(size)
+        self._get_minimum_entropy_position()
 
     def _init_output(self, size):
         """
@@ -99,8 +130,8 @@ class WFCGenerator(object):
         for y in range(size[1]):
             self._output.append([])
             for x in range(size[0]):
-                self._output[-1].append(list(range(len(self._tile_model.patterns))))
-    
+                self._output[-1].append(self._tile_model.patterns)
+
     def __str__(self):
         result = ""
         for line in self._output:
@@ -117,5 +148,6 @@ if __name__ == "__main__":
 
     wfc = WFCGenerator(tm)
     wfc.generate_map((8, 8))
+    exit()
     for line in wfc._output:
         print(line)
