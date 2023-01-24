ed6782cf182dd3fac5e26e8a063857f40453c413
st167058 <st167058@stud.uni-stuttgart.de>
2023-01-24 13:17:54 +0100

    ! fixed critical bug in directions where left/down-most indicies have been forgotten because of using < instead of <=

--- src/directions.py
+++ src/directions.py
@@ -21,8 +21,8 @@ class Directions(enum.Enum):
         img_size should be (row_number, column_number)
         Returns True when the tile is inside the image -> edge cases are removed
         """
-        if pos[0] + self.value[0] >= 0 and pos[0] + self.value[0] < img_size[0] - 1:
-            if pos[1] + self.value[1] >= 0 and pos[1] + self.value[1] < img_size[1] - 1:
+        if pos[0] + self.value[0] >= 0 and pos[0] + self.value[0] <= img_size[0] - 1:
+            if pos[1] + self.value[1] >= 0 and pos[1] + self.value[1] <= img_size[1] - 1:
                 return True
         return False
 
