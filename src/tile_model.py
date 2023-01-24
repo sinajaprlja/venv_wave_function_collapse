d0b7c5ddfce04d0f35da782440c1f02fb4f3a1f4
st167058 <st167058@stud.uni-stuttgart.de>
2023-01-24 13:19:52 +0100

    added len attribute to patterns

--- src/tile_model.py
+++ src/tile_model.py
@@ -22,6 +22,9 @@ class Pattern(object):
     def __str__(self):
         return str(self.index)
     
+    def __len__(self):
+        return 1
+
     def __eq__(self, other):
         return self.pixels == other.pixels
     
