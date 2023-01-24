c8540d1f3f2478dfc8b5094a00fcd0e64d9db281
st167058 <st167058@stud.uni-stuttgart.de>
2023-01-24 01:09:42 +0100

    made rules a dict instead of a list, add hash function to Pattern by using pattern.index

--- src/tile_model.py
+++ src/tile_model.py
@@ -7,17 +7,14 @@ import directions
 from config import *
 
 class Pattern(object):
-    index = 0
+    
     def __init__(self, pixels):
         self.pixels = pixels
         self.probability = None
         self.weight = 1
         self.width = len(pixels[0])
         self.height = len(pixels)
-        self.index = Pattern.index
-        Pattern.index += 1
-        
-
+    
     def set_probability(self, probability: float):
         self.probability = probability
 
@@ -30,6 +27,9 @@ class Pattern(object):
     def __eq__(self, other):
         return self.pixels == other.pixels
     
+    def __hash__(self):
+        return hash(self.index)
+
     def overlaps(self, other, direction):
         """
         Checks if two patterns overlapping parts are equal.
@@ -60,7 +60,7 @@ class TileModel(object):
     def __init__(self, translated_image: image_translator.ImageTranslator):
         self._translated_image = translated_image
         self.patterns = []
-        self.rules = []
+        self.rules = {}
 
     def load(self):
         raise NotImplementedError()
@@ -97,9 +97,8 @@ class TileModel(object):
     def build_patterns(self, pattern_size: int) -> None:
         """
         Get all possible patterns in the translated_image of size <pattern_size> and rotate
-        them 90/180 and 270 degrees
-        Save the corresponding occurance probabilties to probabilities
-        Pattern and probabilty can be identified by their indicies
+        them 90/180 and 270 degrees when option ROTATE is enabled
+        Save the corresponding occurance probabilties to as soon as all distinct patterns have been found
         """
         if pattern_size[0] <= 1 or pattern_size[1] <= 1:
             raise ValueError(f"pattern_size must be at least 2x2, got {pattern_size[0]}x{pattern_size[1]}")
@@ -116,42 +115,42 @@ class TileModel(object):
                     self._add_pattern(pattern)            
 
         weights = sum([pattern.weight for pattern in self.patterns])
-        for pattern in self.patterns:
+        for index, pattern in enumerate(self.patterns):
             pattern.set_probability(pattern.weight / weights)
-        
+            pattern.index = index
 
     def build_rules(self) -> None:
         """
         builds the rules for generating new images by overlapping patterns 
         for every direction(UP, UP_RIGHT, RIGHT, ...) 
         Add rule when overlapping pattern and overlapping questioned pattern are equal
-        rules: list 
-            pattern: dict
+        rules: dict 
+            pattern -> dict
                 direction -> corresponding pattern indicies
         """
-        self._rules = [] 
+        self.rules = {} 
         for pattern in self.patterns:
-            self.rules.append({})
+            self.rules[pattern] = {}
             for direction in directions.Directions:
-                self.rules[-1][direction] = []
+                self.rules[pattern][direction] = []
                 for questioned_pattern in self.patterns:
                     if pattern.overlaps(questioned_pattern, direction):
-                        self.rules[-1][direction].append(self.patterns.index(questioned_pattern))
+                        self.rules[pattern][direction].append(questioned_pattern)
     
     def __str__(self):
         result = "Patterns\n"
-        for index, pattern in enumerate(self.patterns):
-            result = f"{result}  {index}\n    {pattern.pixels}\n"
+        for pattern in self.patterns:
+            result = f"{result}  {pattern.index}\n    {pattern.pixels}\n"
         result = f"{result}\nRules\n"
-        for index, pattern in enumerate(self.rules):
-            result = f"{result}  {index}\n"
-            for direction in pattern:
+        for pattern in self.rules:
+            result = f"{result}  {pattern.index}\n"
+            for direction in self.rules[pattern]:
                 result = f"{result}    {direction}\n"
-                result = f"{result}    - {pattern[direction]}\n"
+                result = f"{result}    - {[x.index for x in self.rules[pattern][direction]]}\n"
         return result
     
     def __len__(self):
-        return sum([len(pattern[direction]) for pattern in self.rules for direction in pattern])
+        return sum([len(self.rules[pattern][direction]) for pattern in self.rules for direction in self.rules[pattern]])
 
 if __name__ == "__main__":
     it = image_translator.ImageTranslator()
