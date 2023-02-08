ae507db550abf58b364fd18460a01fa94557aa4b
st167058 <st167058@stud.uni-stuttgart.de>
2023-02-08 14:41:40 +0100

    removed autosave

--- src/config.py
+++ src/config.py
@@ -22,8 +22,3 @@ MAX_TRIES = 3
 # 3 Get all steps the algorithm takes, include "private" methods 
 DEBUG_LEVEL = 1
 
-# Automatically save translated_image/tile_model as they are static for given input
-# and theres no need to create them multiple times. On the other hand translatation/modeling
-# doesn't really take much resources...
-AUTO_SAVE_TRANSLATED_IMAGE = True
-AUTO_SAVE_TILE_MODEL = True
