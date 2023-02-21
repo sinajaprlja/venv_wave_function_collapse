9dbf148b692039f2187f124863bd5b02a0692a3b
st167058 <st167058@stud.uni-stuttgart.de>
2023-02-21 23:11:31 +0100

    testing stuff, no changes

--- src/wave_function_collapse.py
+++ src/wave_function_collapse.py
@@ -252,7 +252,7 @@ if __name__ == "__main__":
         ["../resources/images/river32x32.png", 1],   
         ["../resources/images/streets32x32.png", 8]
     ]
-    file = 0
+    file = 1
     
     # Translate the input image
     ti = image_translator.ImageTranslator.breakdown_image(*filenames[file])
@@ -265,6 +265,6 @@ if __name__ == "__main__":
     
     # Generate new images via wave function collapse, rebuild the image and save 
     for i in range(1):
-        wfc.generate_map((128, 128))
+        wfc.generate_map((32, 32))
         reversed_map = tile_model.ModelBuilder.reverse_patterns(wfc.output)
-        rebuild_image = image_translator.ImageTranslator.rebuild_image(image_translator.TranslatedImage(ti.tile_map, reversed_map), f"{filenames[file][0][:-3]}result{i}.png")
+        rebuild_image = image_translator.ImageTranslator.rebuild_image(image_translator.TranslatedImage(ti.tile_map, reversed_map), f"{filenames[file][0][:-4]}_result{i}.png")
